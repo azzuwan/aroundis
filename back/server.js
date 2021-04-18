9222bfc6c17670acf725841c83c119f3046513ef
Azzuwan Aziz <azzuwan@gmail.com>
2021-04-18 18:09:48 +0800

    Initial commit. Basic backend and frontend folder. Express and Socket.io setup.

---
+++ back/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(7000, () => {
+  console.log('listening on *:7000');
+});
